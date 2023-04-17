7cdbd0fc3ae4c022286dc2e64bf5d7fd75d4592c
SM Sohag <sohagsheik32@gmail.com>
2023-04-17 20:04:18 +0600

    auth provider context api implements

--- src/providers/AuthProviders.jsx
+++ src/providers/AuthProviders.jsx
@@ -1,10 +1,18 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 
 export const AuthContext = createContext(null);
-const AuthProviders = ({ children }) => {
-  const user = { displayName: "sohag" };
 
-  return <AuthContext.Provider value={user}>{children}</AuthContext.Provider>;
+// auth provider
+const AuthProviders = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  const authInfo = {
+    user,
+  };
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
 };
 
 export default AuthProviders;
