41bc30500350fec25317440605ff2b636e40a1b8
SM Sohag <sohagsheik32@gmail.com>
2023-04-17 20:08:45 +0600

    update

--- src/providers/AuthProviders.jsx
+++ src/providers/AuthProviders.jsx
@@ -1,11 +1,14 @@
 import React, { createContext, useState } from "react";
-
+import app from "../firebase/firebase.config";
+import { getAuth } from "firebase/auth";
+const auth = getAuth(app);
 export const AuthContext = createContext(null);
 
 // auth provider
 const AuthProviders = ({ children }) => {
   const [user, setUser] = useState(null);
 
+  const handleRegister = (email, password) => {};
   const authInfo = {
     user,
   };
